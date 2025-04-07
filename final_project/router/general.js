ca45286bf96cc264e4806447db20a56d14b0bef8
Aayush Mor <aayushmor13@gmail.com>
2025-04-07 14:47:16 -0400

    added the GET by isbn method

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,9 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  isbn = req.params.isbn;
+  book = books[isbn] ;
+  return res.send(book);
  });
   
 // Get book details based on author
