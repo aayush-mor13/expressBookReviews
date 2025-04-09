9875050af9e16a1905634c8becef709d25ef77dc
Aayush <aayushmor13@gmail.com>
2025-04-09 07:18:00 -0400

    Added GET method by author and title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,14 +26,39 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookByAuthor = [];
+
+  for(let isbn in books){
+    if(books[isbn].author === author){
+        bookByAuthor.push({isbn,...books[isbn]});
+    }
+  }
+  if(bookByAuthor.length > 0){
+    return res.json(bookByAuthor);
+  }
+  else{
+    return res.status(404).json("No books found by this author");
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title;
+    let bookByTitle = [];
+
+    for(let isbn in books){
+        if(books[isbn].title === title){
+            bookByTitle.push({isbn,...books[isbn]});
+        }
+    }
+    if(bookByTitle.length > 0){
+        return res.json(bookByTitle);
+    }
+    else{
+        return res.status(300).json({message: "No books found by title"});
+    }
+
 });
 
 //  Get book review
